fb70fc61e0f1c63a3deaefd8124a7689bdc49148
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-01-24 15:43:03 -0700

    adds creating new robot

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,9 +2,20 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+end
